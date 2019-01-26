1f1b1659238e266706ea2553e5a68e7884da27f8
Trung Pham <phambaotrung@gmail.com>
2019-01-26 00:25:15 -0800

    Add shell

--- src/main.rs
+++ src/main.rs
@@ -30,9 +30,9 @@ fn main() {
 	let mut main_opts = Options::new();
     let main_args: Vec<String> = env::args().collect();
     let program = main_args[0].clone();
-    main_opts.optopt("p", "pwd-file", "path to password file (REQUIRED)", "PATH");
-    main_opts.optopt("u", "url", "remote path (ULR) to database", "URL");
-    main_opts.optopt("l", "local-path", "local path to database", "PATH");
+    main_opts.optopt("p", "pwd", "path to password file (REQUIRED)", "PATH");
+    main_opts.optopt("u", "url", "remote path (URL) to database", "URL");
+    main_opts.optopt("l", "local", "local path to database", "PATH");
     main_opts.optflag("h", "help", "show help information");
 
     let main_matches = match main_opts.parse(&main_args[1..]) {
@@ -42,9 +42,13 @@ fn main() {
             std::process::exit(-1);
         },
     };
-    if main_matches.opt_present("h") {
+    if main_matches.opt_present("h") && main_args.len() == 2 {
         print_usage(&program, &main_opts);
         return;
+    }
+    if main_matches.opt_present("h") && main_args.len() != 2 {
+    	print_usage_msg("Error: -h/--help cannot be used with other option(s)", &program, &main_opts);
+        std::process::exit(-1);
     }
 
     let mut enc_buf: Vec<u8> = vec![];
@@ -90,6 +94,56 @@ fn main() {
             }
             enc_file.seek(SeekFrom::Current(-(header.len() as i64))).unwrap();
             enc_file.read_to_end(&mut enc_buf).unwrap();
+        }
+    }
+
+    println!("\nPress ? to get help.");
+    loop {
+		print!("rsec >> ");
+        stdout().flush().unwrap();
+
+     	let mut input = String::new();
+        match stdin().read_line(&mut input) {
+        	Ok(n) => if n <= 1 {
+        		continue;
+        	},
+        	Err(_e) => break,
+        }
+
+        let mut sub_opts = Options::new();
+        let args: Vec<&str> = input.trim().split_whitespace().collect();
+        let cmd = args[0].clone();
+
+        match cmd {
+        	"exit" | "e" | "quit" | "q" => return,
+        	"show" | "sw" => {
+        		sub_opts.optflag("h", "help", "show help information");
+        		sub_opts.optopt("t", "tag", "show entries with the same tag", "TAG");
+
+        		let matches = match sub_opts.parse(&args[1..]) {
+			        Ok(m) => { m },
+			        Err(_f) => { 
+	        			print_usage_msg("Unknown option(s) or missing arg(s).", &cmd, &sub_opts);
+	        			continue;
+			        },
+			    };
+			    if args.len() == 1 {
+	        		print_usage_msg("Unknown option(s) or missing arg(s).", &cmd, &sub_opts);
+	        	} else if matches.opt_present("h") && args.len() == 2 {
+			        print_usage(&cmd, &sub_opts);
+			    } else if matches.opt_present("h") && args.len() != 2 {
+			    	print_usage_msg("Error: -h/--help cannot be used with other option(s)", &cmd, &sub_opts);
+			    } else {
+
+			    }
+        	},
+        	"search" | "sh" => println!("search"),
+        	"?" | "h" => {
+        		println!("show");
+        		println!("search");
+        		println!("add");
+        	},
+        	_ => println!("Unknown command. Press ? to get help."),
         }
     }
 }
